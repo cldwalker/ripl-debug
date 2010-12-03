2760a8c7c02b08c0e38767b81bf94c6295216087
Gabriel Horner <gabriel.horner@gmail.com>
2010-12-03 16:39:24 -0500

    working prototype, handles quirky local vars

---
+++ lib/ripl/debug.rb
@@ -0,0 +1,37 @@
+require 'ruby-debug'
+
+module Ripl
+  module Debug
+    def before_loop
+      super
+      Debugger.run_init_script(StringIO.new)
+    end
+
+    def eval_input(input)
+      if config[:debug]
+        # move local variables outside of with_post_mortem
+        input = (set_var_string = input.to_s[/^\s*\w+\s*=\s+/]) ?
+          "#{set_var_string} with_post_mortem { #{input.sub(set_var_string, '')} }" :
+          "with_post_mortem { #{input} }"
+      end
+      super(input)
+    end
+
+    module Commands
+      # from http://simple-and-basic.com/2009/02/catching-and-examining-exceptions-in-a-irb-session.html
+      def with_post_mortem
+        Debugger.start
+        result = nil
+        Debugger.post_mortem do 
+          result = yield
+        end
+        Debugger.stop
+        result
+      end
+    end
+  end
+end
+
+Ripl::Shell.send :include, Ripl::Debug
+Ripl::Commands.send :include, Ripl::Debug::Commands
+Ripl.config[:debug] = true
